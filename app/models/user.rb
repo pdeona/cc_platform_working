102d3417c9e5c9f28e2cc14a3d7ba7484304a94a
Pedro De Ona <pdeon003@fiu.edu>
2017-09-30 18:30:09 -0400

    user specs added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User
   include Mongoid::Document
+
   field :username
   embeds_many :positions
+
   field :encrypted_password, type: String, encrypted: {random_iv: true}
+
+  validates :username, presence: true
+  validates :password, length: {minimum: 4, maximum: 16}
+  validates :positions, presence: false
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,15 +1,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @u = User.new
+  end
+
   test "it creates a User on .new" do
-    u = User.new
-    assert_instance_of User, u, "Object not an instance of User"
+    assert_instance_of User, @u, "Object not an instance of User"
   end
 
   test "User has a .username method" do
-    u = User.new
-    assert_respond_to u, :username, "User has no .username method"
+    assert_respond_to @u, :username, "User has no .username method"
   end
 
-  test ""
+  test "User has a .password method" do
+    assert_respond_to @u, :password, "User has no .password method"
+  end
+
+  test "User validates :username field" do
+    refute @u.save
+  end
+
+  test "User has an encrypted password field with minimum 4 characters" do
+    invalid_passwords = "too", "toolongheredontdothis", " "
+    invalid_passwords.each do |invalid_password|
+      @u.password = invalid_password
+      refute @u.save
+    end
+  end
+
 end
