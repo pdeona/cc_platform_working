067e6074363f2bf588261a28474e898868bf7d9a
Ed Gonzalez <ergonzalezpmp@gmail.com>
2017-10-01 16:27:22 -0400

    finished transaction model and unit tests.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,4 +5,17 @@ class Transaction
   field :underlying_currency
   field :exchanged_currency
   belongs_to :position
-end
+
+  validates :date, presence: true, format: {with:(/\d{4}\/\d{1,2}\/\d{1,2}/)}
+  validates :underlying_currency, length: { minimum: 3, maximum: 5 }
+  validates :exchanged_currency, length: { minimum: 3, maximum: 5 }
+
+  ACCEPTED_CURRENCIES = ["BTC", "BCH", "ETH", "ETC", "LTC",
+                         "DSH", "XMP", "ZEC", "XRP", "USD",
+                         "EUR", "JPY", "GBP", "CHF", "CAD",
+                         "AUD", "NZD", "ZAR", "CNY"]
+
+  validates_inclusion_of :exchanged_currency, in: ACCEPTED_CURRENCIES
+  validates_inclusion_of :underlying_currency, in: ACCEPTED_CURRENCIES
+
+ end
---
+++ test/models/transaction_test.rb
@@ -0,0 +1,76 @@
+require 'test_helper'
+
+class TransactionTest < ActiveSupport::TestCase
+
+  def setup
+    @p = Position.new
+    @t = Transaction.new(
+              date: "1999/12/19",
+              exchanged_currency: "BTC",
+              underlying_currency: "USD",
+              position: @p
+    )
+  end
+
+  test "it creates a Transaction on .new" do
+    assert_instance_of Transaction, @t, "Object not an instance of Transaction"
+  end
+
+  test "Transaction has a .date method" do
+    assert_respond_to @t, :date, "Transaction has no .date method"
+  end
+
+  test "Transaction has a .position_taken method" do
+    assert_respond_to @t, :position_taken, "Transaction has no .position_taken method"
+  end
+
+  test "Transaction has a .underlying_currency method" do
+    assert_respond_to @t, :underlying_currency, "Transaction has no .underlying_currency method"
+  end
+
+  test "Transaction has a .exchanged_currency method" do
+    assert_respond_to @t, :exchanged_currency, "Transaction has no .exchanged_currency method"
+  end
+
+  test "Transaction has a .position getter method" do
+    assert_respond_to @t, :position, "Transaction has no .position method"
+  end
+
+  test "Transaction has a .position setter method" do
+    assert_respond_to @t, :position=, "Transaction has no .position= method"
+  end
+
+  test "Transaction validates position_id" do
+    @t.position = nil
+    refute @t.save, "Transaction was saved without a position_id"
+  end
+
+  test "Transaction saves with valid position_id" do
+    assert @t.save, "Transaction was unable to save"
+  end
+
+  test "The date attribute only accepts accepted formats" do
+    @t.date = 'invalid'
+    refute @t.save, "transaction was saved with invalid date"
+  end
+
+  test "The exchanged_currency attribute validates symbol length" do
+    @t.exchanged_currency = "invalid"
+    refute @t.save, "Transaction was saved with invalid exchanged_currency lenth"
+  end
+
+  test "The underlying_currency attribute validates symbol length" do
+    @t.underlying_currency = "invalid"
+    refute @t.save, "Transaction was saved with invalid underlying_currency length"
+  end
+
+  test "The exchanged_currency attribute validates symbol matches an existing currency" do
+    @t.exchanged_currency = "123"
+    refute @t.save, "Transaction saved with invalid exchanged_currency symbol"
+  end
+
+  test "The underlying_currency attribute validates symbol matches an existing currency" do
+    @t.underlying_currency = "123"
+    refute @t.save, "Trnasaction saved with invalid exchanged_currency symbol"
+  end
+end
